f5e0c57ae31218dd2dd47398c614646830e32406
Liz W <lizwigg6@gmail.com>
2015-07-02 16:11:55 -0500

    Change colorDisplay stuff and colors

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 var tilesArray = [];
-var colors = ["redTile", "pinkTile", "purpleTile", "blueTile", "greenTile", "orangeTile"];
-var textColors = ["red", "pink", "purple", "blue", "green", "orange"];
+var colors = ["red", "pink", "purple", "blue", "green", "orange"];
 var score = 0;
 var time = 0;
 
@@ -20,6 +19,8 @@ function drawTiles() {
 			//  Add a random color class
 			var randCol = colors[Math.floor(Math.random() * 6)];
 			cell.className += "tile " + randCol;
+			//  Add an onclick event
+			//cell.onclick = checkTile;
 			//  Add the cell to the row
 			row.appendChild(cell);
 		}
@@ -31,12 +32,17 @@ function drawTiles() {
 //  Changes the color you're supposed to click on
 function changeCurrentColor() {
 	//  Randomize the color
-	var randCol = textColors[Math.floor(Math.random() * 6)];
+	var randCol = colors[Math.floor(Math.random() * 6)];
 	//  Update the color displayer
 	var colorDisplayer = document.getElementById('colorDisplay');
 	colorDisplayer.innerHTML = 'Click on the <span class="colorDisplay ' + randCol + '">' + randCol + '</span> tiles.';
 	//colorDisplayer.className += randCol;
 }
+
+//  Handles events when the player clicks on a tile
+/*function checkTile() {
+
+}*/
 
 //  Randomize all the tile colors
 /*function shuffleColors() {
@@ -51,18 +57,6 @@ function timeHandler() {
 	//  Check score tier
 	//  If score is a certain amount
 	//  Run shuffleColors every x seconds
-}
-
-//  Handles events when the player clicks on a tile
-function checkTile() {
-	if (the clicked tile matches the current color) {
-		//  Add points to the score
-		//  Update the score text
-		//  Change the tile color
-	} else {
-		//  Take away a life
-		//  Update the life counter
-	}
 }*/
 
 drawTiles();
